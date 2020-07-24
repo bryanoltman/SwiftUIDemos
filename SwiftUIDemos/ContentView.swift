8a6d4c525d4b47dc5fd3d28a44dacb0bb29c810d
Bryan Oltman <bryanoltman@gmail.com>
2020-07-23 21:03:55 -0400

    Add The SwiftUI Lab text animation example

--- SwiftUIDemos/ContentView.swift
+++ SwiftUIDemos/ContentView.swift
@@ -7,22 +7,32 @@ struct ContentView: View {
   var body: some View {
     NavigationView {
       List {
-        Button("Modal Dismissal") {
-          self.showingProgrammaticDismissalViewDemo.toggle()
-        }
-        .sheet(isPresented: $showingProgrammaticDismissalViewDemo) {
-          ProgrammaticDismissalView()
-        }
-
-        Button("Navigation in Modal") {
-          self.showingSheetNavigationViewDemo.toggle()
-        }
-        .sheet(isPresented: $showingSheetNavigationViewDemo) {
-          SheetNavigationView(showSheetView: self.$showingSheetNavigationViewDemo)
+        Section(header: Text("Animation")) {
+          NavigationLink(destination: TextColorAnimationDemoView()) {
+            Text("Text Color Animation Demo")
+          }
         }
 
-        NavigationLink(destination: TextColorAnimationDemoView()) {
-          Text("Text Color Animation Demo")
+        Section(header: Text("The SwiftUI Lab")) {
+          NavigationLink(destination: Example12()) {
+            Text("Letter size animation")
+          }
+        }
+
+        Section(header: Text("Navigation")) {
+          Button("Modal Dismissal") {
+            self.showingProgrammaticDismissalViewDemo.toggle()
+          }
+          .sheet(isPresented: $showingProgrammaticDismissalViewDemo) {
+            ProgrammaticDismissalView()
+          }
+
+          Button("Navigation in Modal") {
+            self.showingSheetNavigationViewDemo.toggle()
+          }
+          .sheet(isPresented: $showingSheetNavigationViewDemo) {
+            SheetNavigationView(showSheetView: self.$showingSheetNavigationViewDemo)
+          }
         }
       }
       .navigationBarTitle("Demos")
